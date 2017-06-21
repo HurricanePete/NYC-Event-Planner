5414f7a7822453c2cb4c258be99014b08fb84c10
HurricanePete <zpete725@gmail.com>
2017-06-21 14:33:55 -0400

    Add of map and collapsable elements
    -add of Google map but does not currently display
    -add collapsable functionalty for result-panes
    -add of future geocoding function but no functionality

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ var PERMITTED_EVENT_ENDPOINT = 'https://data.cityofnewyork.us/resource/8end-qv57
 
 var RESULT_HTML_TEMPLATE = (
   '<div class="result-panes">' +
-    '<div><button class="results-button"><span class="js-event-name"></span><br><span class="js-start-date"></span></button></div>' +
-    '<div class="results-collapse hidden"><ul><li class="js-end-time"></li><li class="js-event-loc"></li><li class="js-event-borough"></li><li class="js-event-type"></li><li class="js-event-agency"></li></ul></div>' +
+    '<button class="results-button"><span class="js-event-name"></span><br><span class="js-start-date"></span></button>' +
+    '<div class="results-collapse hidden" id="collapse"><ul><li class="js-end-time"></li><li class="js-event-loc"></li><li class="js-event-borough"></li><li class="js-event-type"></li><li class="js-event-agency"></li></ul></div>' +
   '</div>'
 );
+
+function initializeMap(state) {
+  var latlng = new google.maps.LatLng(40.7288, -73.9579);
+  var mapOptions = {
+   zoom: 8,
+   center: latlng,
+   mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+function codeAddress(state) {
+  var address = document.getElementById('address').value;
+  state.geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      //state.map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: state.map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 function getCurrentDate () {
   var today = new Date();
@@ -43,8 +67,8 @@ function getDataFromApi(callback) {
 function renderResult(result) {
   var template = $(RESULT_HTML_TEMPLATE);
   template.find(".js-event-name").text("Event Name: " + result.event_name);
-  template.find(".js-start-date").text("Starts: " + result.start_date_time);
-  template.find(".js-end-time").text("Ends: " + result.end_date_time);
+  template.find(".js-start-date").text("Starts: " + (result.start_date_time).substring(0,16));
+  template.find(".js-end-time").text("Ends: " + (result.end_date_time).substring(0,16));
   template.find(".js-event-loc").text("Location: " + result.event_location);
   template.find(".js-event-borough").text("Borough: " + result.event_borough);
   template.find(".js-event-type").text("Type: " + result.event_type);
@@ -59,9 +83,6 @@ function displayEventData(data) {
   $('.js-result-display').html(results);
 }
 
-
-
-
 $('div.button-nav').on('click', '#approved-events', function (event) {
     event.preventDefault();
     getDataFromApi(displayEventData);
@@ -73,5 +94,5 @@ $('div.button-nav').on('click', '#approved-events', function (event) {
 
 $('div.js-result-display').on('click', '.results-button', function (event) {
   event.preventDefault();
-  $(this).find('.results-collapse').toggleClass("hidden");
+  $(this).closest('div').find('.results-collapse').toggleClass('hidden');
 })
