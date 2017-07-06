ce78ba4dc28a5cf7f89d4b8b5d4a30b75278ca73
HurricanePete <zpete725@gmail.com>
2017-07-06 13:21:32 -0400

    add of function to store data from NYC open data text files - not yet functional

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-=======
 var state = {
-  map: null
+  map: null,
+  offset: 0,
+  searchTerm: null,
+  currentApiData: null
 }
 
 function endpointSwitcher (selection) {
@@ -21,7 +22,6 @@ function endpointSwitcher (selection) {
   return API_ENDPOINT;
 }
 
->>>>>>> test-branch
 var PERMITTED_EVENT_ENDPOINT = 'https://data.cityofnewyork.us/resource/8end-qv57.json'
 
 var RESULT_HTML_TEMPLATE = (
@@ -30,12 +30,6 @@ var RESULT_HTML_TEMPLATE = (
     '<div class="results-collapse hidden" id="collapse"><ul><li class="js-start-date"></li><li class="js-end-time"></li><li class="js-event-loc"></li><li class="js-event-borough"></li><li class="js-event-agency"></li></ul></div>' +
   '</div>'
 );
-
-var state = {
-  map: null,
-  offset: 0,
-  searchTerm: null
-}
 
 function navNext(state) {
   state.offset += 6;
@@ -48,7 +42,7 @@ function navPrev(state) {
 function initializeMap() {
   var latlng = new google.maps.LatLng(40.7288, -73.9579);
   var mapOptions = {
-   zoom: 11,
+   zoom: 10,
    center: latlng
   }
   state.map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -73,6 +67,15 @@ function getDataFromApi(callback, state ) {
   $.ajax(settings);
 }
 
+//function to fetch and store this data as an array within the state object
+$.get('playgrounds.txt', function(data, state) {
+  state.currentApiData = data;
+})
+
+function storeApiData(data, state) {
+  state.currentApiData = data;
+}
+
 function renderResult(result) {
   var template = $(RESULT_HTML_TEMPLATE);
   template.find(".js-event-name").text("Event Name: " + result.event_name);
@@ -93,16 +96,22 @@ function displayEventData(data) {
 }
 
 function createAddress(data) {
-  var results = data.map(function(item, index) {
-    return (item.event_location.substring(0,40) + ', ' + item.event_borough);
-  });
- return results;
+  try {
+    var results = data.map(function(item, index) {
+      return (item.event_location.substring(0,40) + ', ' + item.event_borough);
+    });
+    return results;
+  }
+  catch (err) {
+    return;
+  }
 }
 
 function codeAddress(data) {
   var geocoder = new google.maps.Geocoder();
   var address = createAddress(data);
-  address.forEach(function(item, index){geocoder.geocode( { 'address': item}, function(results, status) {
+  try {
+    address.forEach(function(item, index){geocoder.geocode( { 'address': item}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         var marker = new google.maps.Marker({
             map: state.map,
@@ -118,6 +127,10 @@ function codeAddress(data) {
       }
     })
   })
+  }
+  catch (err) {
+    return;
+  }
 }
 
 function displayApiResults(target) {
@@ -146,11 +159,17 @@ function initializeGoogle () {
 }
 
 $('div.button-nav').on('click', '#approved-events', function (event) {
-    event.preventDefault();
-    getDataFromApi(filterApiData, state);
-    displayApiResults($(this));
-    initializeMap();
-    console.log('You did it');
+  event.preventDefault();
+  getDataFromApi(filterApiData, state);
+  displayApiResults($(this));
+  initializeMap();
+  console.log('You did it');
+})
+
+$('div.button-nav').on('click', '#playgrounds', function (event) {
+  event.preventDefault();
+  textDataFromApi(storeApiData, 2);
+  console.log('Success');
 })
 
 $('div.js-result-display').on('click', '.results-button', function (event) {
