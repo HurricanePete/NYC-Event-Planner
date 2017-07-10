fbef23ce1b26dfc28f88d6e4c23203ba4c9535d2
HurricanePete <zpete725@gmail.com>
2017-07-10 15:51:19 -0400

    add of key features and style changes
    -add of state key to hold NY open data api call data
    -add of state key to hold geocode data from google api call but not
    functional
    -add if statement to remove borough from NY open data api call if no
    borough selected
    -add event listener to initialize map on page load
    -add switch function to select map options based on borough selection
    -add function to add map markers but not yet functional
    -add error message to display if no results are found

--- app.js
+++ app.js
@@ -1,68 +1,191 @@
 var state = {
   map: null,
   offset: 0,
+  savedOffset: 0,
   searchTerm: null,
   borough: null,
-  savedResults: []
+  savedResults: [],
+  geocodeResults: []
 }
 
 var PUBLIC_RESTROOMS_ENDPOINT = 'https://data.cityofnewyork.us/resource/r27e-u3sy.json'
 
 var RESULT_HTML_TEMPLATE = (
-	'<div class="br-results"' +
+	'<div class="br-results">' +
 	'<div><h3 class="js-br-title"></h3></div>' + 
-	'<div><p class="js-br-comment></p><br>' + 
+	'<div><p class="js-br-comment"></p><br>' + 
 	'<p class="js-br-open"></p><br>' + 
 	'<p class="js-br-borough"></p><br>' +
-	'<p class="js-br-location"</p><br>' +
+	'<p class="js-br-location"></p><br>' +
 	'<img class="js-handicap hidden" src="handicap.jpg">' +
+	'<img class="js-no-handicap hidden" src="nohandicap.jpg">' +
 	'</div>'
 );
 
+var RESULT_FAILURE_TEMPLATE = (
+	'<div class="br-results">' +
+	'<h3>Oops, there\'s nothing here</h3>' +
+	'<h4>Hold for a bit longer and try a different search or different borough</h4>' +
+	'</div>'
+	)
+
+function setSavedResults (data) {
+	state.savedResults = data;
+}
+
+function pickMapCenter (state) {
+	var mapOptions = null;
+	try {
+		switch (state.borough) {
+			case "Staten Island":
+				mapOptions = {
+					zoom: 13,
+					center: new google.maps.LatLng(40.580725, -74.152862)
+				}
+				break;
+			case "Brooklyn":
+				mapOptions = {
+					zoom: 13,
+					center: new google.maps.LatLng(40.654559, -73.930133)
+				}
+				break;
+			case "Manhattan":
+				mapOptions = {
+					zoom: 13,
+					center: new google.maps.LatLng(40.779812, -73.966761)
+				}
+				break;
+			case "Bronx":
+				mapOptions = {
+					zoom: 13,
+					center: new google.maps.LatLng(40.853882, -73.877219)
+				}
+				break;
+			case "Queens":
+				mapOptions = {
+					zoom: 13,
+					center: new google.maps.LatLng(40.746069, -73.846646)
+				}
+				break;
+			default:
+				mapOptions = {
+					zoom: 12,
+					center: new google.maps.LatLng(40.707149, -74.012314)
+				} 		
+		}
+		return mapOptions;
+	}
+	catch (err) {
+		console.log("switch failure");
+		return;
+	}
+}
+
+function initializeMap(state) {
+	var mapOptions = pickMapCenter(state);
+	state.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
 function getDataFromApi(callback, state) {
-  var settings = {
-    url: PUBLIC_RESTROOMS_ENDPOINT,
-    data: {
-      '$$app_token': '4buJLe3e35CTn7IkRQcSZ8i3W',
-      'borough': state.borough,
-      '$limit': 20,
-      '$q' : state.searchTerm,
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback
+  if (state.borough === "") {
+  	var settings = {
+    	url: PUBLIC_RESTROOMS_ENDPOINT,
+    	data: {
+      	'$$app_token': '4buJLe3e35CTn7IkRQcSZ8i3W',
+      	'$limit': 20,
+      	'$offset': state.offset,
+      	'$q' : state.searchTerm,
+    	},
+    	dataType: 'json',
+    	type: 'GET',
+    	success: callback
+  	}
   }
-    
+  else {
+  	var settings = {
+    	url: PUBLIC_RESTROOMS_ENDPOINT,
+    	data: {
+      	'$$app_token': '4buJLe3e35CTn7IkRQcSZ8i3W',
+      	'$limit': 20,
+      	'$offset': state.offset,
+      	'borough': state.borough,
+      	'$q' : state.searchTerm,
+    	},
+    	dataType: 'json',
+    	type: 'GET',
+    	success: callback
+  	}
+  }
   $.ajax(settings);
 }
 
-function setSavedResults(data, state) {
-	state.savedResults = data;
-	console.log(data);
-	//displayBrData(state);
+function createAddress(state) {
+  try {
+    var results = state.savedResults.map(function(item, index) {
+      return (item.name + ', New York');
+    });
+    return results;
+  }
+  catch (err) {
+    return;
+  }
+}
+
+function codeAddress(state) {
+	var geocoder = new google.maps.Geocoder();
+	var address = createAddress(state);
+	state.geocodeResults = address.map(function(item, index){geocoder.geocode( { 'address': item}, function(results, status) {
+   		if (status == google.maps.GeocoderStatus.OK) {
+   			console.log("success")
+   		}
+   		else {
+   			console.log("fail" + item)
+      		}
+		})
+	})
 }
 
 function renderResult(result) {
   var template = $(RESULT_HTML_TEMPLATE);
   template.find(".js-br-title").text(result.name);
-  template.find(".js-br-comments").text(result.comments);
-  template.find(".js-br-open").text(result.open_year_round);
+  template.find(".js-br-comment").text(result.comments);
+  template.find(".js-br-open").text("Open Year Round: " + result.open_year_round);
   template.find(".js-br-borough").text(result.borough);
   template.find(".js-br-location").text(result.location);
-  if (result.handicap_accessible == 'Yes') {
+  if (result.handicap_accessible === 'Yes') {
   	template.find(".js-handicap").removeClass("hidden");
   }
-  else {
-  	return;
+  if (result.handicap_accessible === undefined) {
+  	template.find(".js-no-handicap").removeClass("hidden");
   }
   return template;
 }
 
 function displayBrData(data) {
-	var results = data.map(function(item, index) {
-    	return renderResult(item);
-  	});
-  $('.js-result-display').html(results);
+	if (data[0] != null) {
+		setSavedResults(data);
+		var results = data.map(function(item, index) {
+    		return renderResult(item);
+    	})
+    }
+	else {
+		var results = RESULT_FAILURE_TEMPLATE;
+	}
+	$('.js-result-display').html(results);
+}
+
+function createMapMarkers(state) {
+	for (i=0; i<10; i++) {
+		var marker = new google.maps.Marker({
+			position: state.geocodeResults[i],
+		});
+		marker.setMap(state.map);
+	}
+}
+
+function initializeGoogle () {
+  initializeMap(state);
+  codeAddress(state);
 }
 
 function displayApiResults(target) {
@@ -71,10 +194,18 @@ function displayApiResults(target) {
   target.closest('body').find('div.results').removeClass('hidden');
 }
 
+$(document).ready(function(){
+	initializeMap(state);
+})
+
 $('form').submit(function(event) {
   event.preventDefault();
   state.searchTerm = $('#search').val();
   state.borough = $('input[name="borough"]:checked').val();
   getDataFromApi(displayBrData, state);
   displayApiResults($(this));
+  codeAddress(state);
+  createMapMarkers(state);
+  initializeMap(state);
+  google.maps.event.trigger(map, "resize");
 })
