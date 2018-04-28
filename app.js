2815b2fa520cbe5144bb83498ede8438b6c7e2fd
HurricanePete <zpete725@gmail.com>
2018-04-28 16:54:43 -0400

    add of additional bug fixes for Pixel code

--- app.js
+++ app.js
@@ -182,6 +182,7 @@ function displayApiResults(target) {
 
 $('form').submit(function(event) {
   event.preventDefault();
+  fbq('track', 'Search');
   resetOffset(state);
   state.searchTerm = cleanInput($('#search').val());
   state.borough = $('input[name="borough"]:checked').val();
